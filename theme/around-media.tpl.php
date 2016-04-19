37bdbdea8c3911b94450c09cf334708a3f240500
Angelo Vanmarcke <angelo@maister.be>
2016-04-19 09:21:57 +0200

    Initial commit.

---
+++ theme/around-media.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Stub template file to call the main FlexSlider theme
+ *
+ * We can't set the Views display to use the other theme directly since it
+ * agressively changes settings of the core theme function if we do.
+ * So we have to have this stub theme instead.
+ *
+ * @author Mathew Winstone <mwinstone@coldfrontlabs.ca>
+ */
+print theme('around_media', array('items' => $items, 'settings' => $settings));
